80e9e195b3bc2b9be83eb57dcc6d3f8601a8ca47
JoRE13 <joi.buland@gmail.com>
2026-03-20 14:12:36 +0000

    add seat

---
+++ src/main/java/airline/model/Airplane.java
@@ -0,0 +1,11 @@
+package src.main.java.airline.model;
+
+public class Airplane {
+    private String type;
+    private int rows;
+
+    public void Airplane(String type, int rows) {
+        this.type = type;
+        this.rows = rows;
+    }
+}
---
+++ src/main/java/airline/model/Seat.java
@@ -0,0 +1,23 @@
+package src.main.java.airline.model;
+
+public class Seat {
+    private String seatID;
+    private String seatType;
+    private Boolean isAvailable;
+
+    public void Seat(String seatID, String seatType) {
+        this.seatID = seatID;
+        this.seatType = seatType;
+        this.isAvailable = true;
+    }
+
+    public Boolean isWindow() {
+        // gera is window, hvernig skilgreinum við seatType, kannski enum?
+        return false;
+    }
+
+    public Boolean isAisle() {
+        // gera is aisle, hvernig skilgreinum við seatType, kannski enum?
+        return false;
+    }
+}
