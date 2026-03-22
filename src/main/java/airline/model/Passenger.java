87a65f09b704a4744b859acec32aa8eb6d4eba57
Andrea E <ane28@hi.is>
2026-03-22 17:12:40 +0000

    passenger getters og setters

--- src/main/java/airline/model/Passenger.java
+++ src/main/java/airline/model/Passenger.java
@@ -22,6 +22,45 @@ public class Passenger {
         this.dateOfBirth = dateOfBirth;
     }
 
+    //Getters
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    //setters
+    public  void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public void setNationality(String nationality){
+        this.nationality = nationality;
+    }
+
+    //methods
+    public void setDateOfBirth(Date dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
     public void updateContactInfo(String email, String phone){
         this.email = email;
         this.phone = phone;
