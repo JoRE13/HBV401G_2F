017d904449164664cd9e438d92b8f7abfb30275d
Andrea E <ane28@hi.is>
2026-03-22 17:19:54 +0000

    ReservationItem

---
+++ src/main/java/airline/model/ReservationItem.java
@@ -0,0 +1,34 @@
+package src.main.java.airline.model;
+
+public class ReservationItem {
+    private String itemId;
+    private double pricePaid;
+
+    public ReservationItem(
+            String itemId,
+            double pricePaid){
+        this.itemId = itemId;
+        this.pricePaid = pricePaid;
+    }
+
+    //getters
+    public String getItemId(){
+        return itemId;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+    //setters
+    public void setItemId(String itemId){
+        this.itemId = itemId;
+    }
+    public void setPricePaid(double pricePaid) {
+        this.pricePaid = pricePaid;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
