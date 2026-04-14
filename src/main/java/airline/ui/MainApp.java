349055010c1d2e728a0184257717c57f7aef6a30
Andrea E <ane28@hi.is>
2026-04-14 20:17:30 +0000

    SearchFlight UI - gluggi til þess að leita að flugum

---
+++ src/main/java/airline/ui/MainApp.java
@@ -0,0 +1,23 @@
+package airline.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/flight-search.fxml"));
+        Scene scene = new Scene(loader.load(), 900, 600);
+
+        stage.setTitle("Flight Search");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
